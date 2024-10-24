88e895ab77ee3bd16f8f3d064592104c67da27bf
nan-wang <nan.wang@jina.ai>
2024-10-24 10:07:41 +0800

    feat: clean up ch05-01

--- ch05/01_prepare-data/naive_rag.py
+++ ch05/01_prepare-data/naive_rag.py
@@ -1,24 +1,22 @@
-import sys
-import json
+from pathlib import Path
 
 import dotenv
-from pathlib import Path
 from langchain import hub
 from langchain_chroma import Chroma
-from langchain_core.documents import Document
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough, RunnableParallel, RunnablePick
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-from utils import load_documents, get_chunks, format_docs, split_contexts
+
+from utils import load_documents, get_chunks, format_docs
 
 dotenv.load_dotenv()
 
 vector_db_dir = '../data_chroma'
 collection_name = 'test_db'
 
-
 if Path(vector_db_dir).exists():
-    vectorstore = Chroma(persist_directory=vector_db_dir, embedding_function=OpenAIEmbeddings(), create_collection_if_not_exists=False, collection_name=collection_name)
+    vectorstore = Chroma(persist_directory=vector_db_dir, embedding_function=OpenAIEmbeddings(),
+                         create_collection_if_not_exists=False, collection_name=collection_name)
     print(f"Loaded {vectorstore._chroma_collection.count()} documents")
 else:
     # walk through the text files under "data" directory
@@ -29,24 +27,12 @@ else:
     print(f"Split the documents into {len(chunks)} chunks")
 
     vectorstore = Chroma.from_documents(
-        documents=chunks, embedding=OpenAIEmbeddings(), persist_directory=vector_db_dir, collection_name=collection_name)
+        documents=chunks, embedding=OpenAIEmbeddings(), persist_directory=vector_db_dir,
+        collection_name=collection_name)
 
 retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 5})
 
 query = "奥运火炬传递到达过珠峰吗?"
-# query = "奥运会是什么时候开始停止支持4:3全屏转播的?"
-# query = "中国在越野滑雪项目中的表现怎么样?"
-# query = "2024巴黎奥运会有棒球么?"
-# query = "2020奥运会有哪些兴奋剂相关新闻?"
-# query = "介绍北京申办奥运会的历史"
-# retrieved_docs = retriever.invoke(query)
-#
-# print(f"Retrieved {len(retrieved_docs)} documents")
-# for doc in retrieved_docs:
-#     print(f"Retrieved doc, {repr(doc.page_content)}")
-#     # print(f"Retrieved doc meta, {doc.metadata}")
-# exit(0)
-
 
 llm = ChatOpenAI(model="gpt-4o-2024-08-06")
 prompt = hub.pull("rlm/rag-prompt")
@@ -54,96 +40,10 @@ prompt = hub.pull("rlm/rag-prompt")
 rag_chain = (
         {"context": retriever | format_docs, "question": RunnablePassthrough()}
         | RunnableParallel(
-            contexts=RunnablePick("context"),
-            question=RunnablePick("question"),
-            answer=prompt | llm | StrOutputParser())
+    contexts=RunnablePick("context"),
+    question=RunnablePick("question"),
+    answer=prompt | llm | StrOutputParser())
 )
-
 
 result = rag_chain.invoke(query)
-print(result)
 print(result['answer'])
-
-sys.exit(0)
-
-# load the evaluation data from `data_eval/qa_pairs.json`
-examples = []
-with open("data_eval/qa_pairs.json", "r") as f:
-    qa_pairs = json.load(f)
-    for qa_pair in qa_pairs:
-        query = qa_pair["question"]
-        ground_truth = qa_pair["answer"]
-        source_documents = qa_pair["documents"]
-        examples.append(
-            {
-                "query": query,
-                "ground_truth": ground_truth,
-                "source_documents": source_documents,
-            }
-        )
-
-# from ragas.integrations.langchain import EvaluatorChain
-# from ragas.metrics import (
-#     faithfulness,
-#     answer_relevancy,
-#     context_precision,
-#     context_recall,
-# )
-#
-# faithfulness_chain = EvaluatorChain(metric=faithfulness)
-# answer_relevancy_chain = EvaluatorChain(metric=answer_relevancy)
-# context_precision_chain = EvaluatorChain(metric=context_precision)
-# context_recall_chain = EvaluatorChain(metric=context_recall)
-
-question = []
-answer = []
-ground_truth = []
-contexts = []
-for example in examples[:1]:
-    q = example["query"]
-    result = rag_chain.invoke(q)
-    question.append(q)
-    answer.append(result["answer"])
-    ground_truth.append(example["ground_truth"])
-    contexts.append(split_contexts(result["contexts"]))
-
-data = {
-    "question": question,
-    "answer": answer,
-    "ground_truth": ground_truth,
-    "contexts": contexts,
-}
-
-from ragas import evaluate
-from ragas.metrics import (
-    faithfulness,
-    answer_relevancy,
-    context_recall,
-    context_precision,
-)
-
-from datasets import Dataset
-
-dataset = Dataset.from_dict(data)
-
-answer_relevancy.llm = None
-context_recall.llm = None
-context_precision.llm = None
-answer_relevancy.embeddings = None
-context_recall.embeddings = None
-context_precision.embeddings = None
-
-result = evaluate(
-    dataset=dataset,
-    metrics=[
-        context_precision,
-        context_recall,
-        answer_relevancy,
-    ],
-    llm=llm,
-    embeddings=OpenAIEmbeddings(),
-)
-
-eval_df = result.to_pandas()
-
-eval_df.to_json("eval_results.json", orient="records", indent=4, force_ascii=False)
