25f54a2bb3024d4d706ba32c4bd8d8d82fbb3284
nan-wang <nan.wang@jina.ai>
2024-12-19 15:53:24 +0800

    chore: clean up

--- ch05/01_prepare-data/naive_rag.py
+++ ch05/01_prepare-data/naive_rag.py
@@ -1,22 +1,26 @@
 from pathlib import Path
 
 import dotenv
-from langchain import hub
+from langchain.prompts import ChatPromptTemplate
 from langchain_chroma import Chroma
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough, RunnableParallel, RunnablePick
+from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
 from utils import load_documents, get_chunks, format_docs
 
 dotenv.load_dotenv()
 
-vector_db_dir = '../data_chroma'
-collection_name = 'test_db'
+VECTOR_DB_DIR = '../data_chroma_test_db'
+COLLECTION_NAME = 'olympic_games'
 
-if Path(vector_db_dir).exists():
-    vectorstore = Chroma(persist_directory=vector_db_dir, embedding_function=OpenAIEmbeddings(),
-                         create_collection_if_not_exists=False, collection_name=collection_name)
+
+if Path(VECTOR_DB_DIR).exists():
+    vectorstore = Chroma(
+        persist_directory=VECTOR_DB_DIR,
+        embedding_function=OpenAIEmbeddings(),
+        create_collection_if_not_exists=False,
+        collection_name=COLLECTION_NAME)
     print(f"Loaded {vectorstore._chroma_collection.count()} documents")
 else:
     # walk through the text files under "data" directory
@@ -27,23 +31,28 @@ else:
     print(f"Split the documents into {len(chunks)} chunks")
 
     vectorstore = Chroma.from_documents(
-        documents=chunks, embedding=OpenAIEmbeddings(), persist_directory=vector_db_dir,
-        collection_name=collection_name)
+        documents=chunks, embedding=OpenAIEmbeddings(), persist_directory=VECTOR_DB_DIR,
+        collection_name=COLLECTION_NAME)
 
 retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 5})
 
-query = "奥运火炬传递到达过珠峰吗?"
 
 llm = ChatOpenAI(model="gpt-4o-2024-08-06")
-prompt = hub.pull("rlm/rag-prompt")
+prompt = ChatPromptTemplate.from_template(
+    """You are an assistant for question-answering tasks. 
+Use the following pieces of retrieved context to answer the question. 
+If you don't know the answer, just say that you don't know. 
+Use three sentences maximum and keep the answer concise.
+Question: {question} 
+Context: {context} 
+Answer:
+""")
 
 rag_chain = (
         {"context": retriever | format_docs, "question": RunnablePassthrough()}
-        | RunnableParallel(
-    contexts=RunnablePick("context"),
-    question=RunnablePick("question"),
-    answer=prompt | llm | StrOutputParser())
+        | prompt | llm | StrOutputParser()
 )
 
+query = "2024年巴黎奥运会的开幕式是哪一天?"
 result = rag_chain.invoke(query)
-print(result['answer'])
+print(result)
