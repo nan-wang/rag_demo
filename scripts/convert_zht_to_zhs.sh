ec927b45bc69a53137003d1775be3c529a2e2597
nan-wang <nan.wang@jina.ai>
2025-02-01 22:51:48 +0800

    fix: fix the missmatch between traditional and simple Chinese

--- scripts/convert_zht_to_zhs.sh
+++ scripts/convert_zht_to_zhs.sh
@@ -2,6 +2,8 @@
 
 # Convert Traditional Chinese to Simplified Chinese using opencc
 # loop through all files in the directory `data` and convert them into Simplified Chinese and save them in the directory `data_zh`
+# replace `雪梨` with `悉尼` in the converted files
+# convert the file names to simplified chinese
 for file in data/*.txt; do
     opencc -c t2s -i $file -o data_zh/$(basename $file)
 done
