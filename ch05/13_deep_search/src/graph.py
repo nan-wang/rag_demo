0d44fef6cf896b4f8c759dc8e6d2461d3acc6b15
nan-wang <nan.wang@jina.ai>
2025-04-16 07:17:22 +0800

    feat: add local search

--- ch05/13_deep_search/src/graph.py
+++ ch05/13_deep_search/src/graph.py
@@ -1,6 +1,15 @@
 import json
+from pathlib import Path
 from typing import Literal
 
+import dotenv
+import pkuseg
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers import EnsembleRetriever
+from langchain_chroma import Chroma
+from langchain_community.document_compressors.jina_rerank import JinaRerank
+from langchain_community.embeddings import JinaEmbeddings
+from langchain_community.retrievers import BM25Retriever
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.runnables import RunnableConfig
 from langchain_openai import ChatOpenAI
@@ -9,10 +18,59 @@ from langgraph.graph import StateGraph, START, END
 from configuration import Configuration
 from prompts import query_writer_instructions, get_current_date, summarizer_instructions, reflection_instructions
 from state import SummaryState, SummaryStateInput, SummaryStateOutput
-from utils import strip_thinking_tokens, get_config_value, tavily_search, format_sources, deduplicate_and_format_sources, extract_json_from_markdown
-import dotenv
+from utils import strip_thinking_tokens, get_config_value, tavily_search, format_sources, \
+    deduplicate_and_format_sources, extract_json_from_markdown, load_documents, split_chunks, split_sections
 
 dotenv.load_dotenv()
+
+
+def get_all_splits():
+    docs = load_documents("../../data/*.txt")
+    print(f"Loaded {len(docs)} documents")
+    chunks = []
+    for doc in docs:
+        text = doc.page_content
+        article_title = Path(doc.metadata.get("source", "")).stem
+        sections = split_sections(text, source=article_title)
+        _chunks = split_chunks(sections)
+        chunks.extend(_chunks)
+    return chunks
+
+
+vector_db_dir = '../../data_chroma_jina_embeddings'
+collection_name = 'olympic_games'
+if Path(vector_db_dir).exists():
+    vectorstore = Chroma(
+        persist_directory=vector_db_dir,
+        embedding_function=JinaEmbeddings(model_name="jina-embeddings-v3"),
+        create_collection_if_not_exists=False,
+        collection_name=collection_name)
+    print(f"{vectorstore._chroma_collection.count()} documents loaded")
+vector_retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 10})
+
+seg = pkuseg.pkuseg()
+
+def tokenize_doc(doc_str: str):
+    result = []
+    for l in doc_str.splitlines():
+        ll = l.strip()
+        if not ll:
+            continue
+        split_tokens = [t.strip() for t in seg.cut(ll) if t.strip() != '']
+        result += split_tokens
+    return result
+
+
+chunks = get_all_splits()
+bm25_retriever = BM25Retriever.from_documents(
+    chunks, preprocess_func=tokenize_doc)
+bm25_retriever.k = 10
+ensemble_retriever = EnsembleRetriever(retrievers=[vector_retriever, bm25_retriever], weights=[0.5, 0.5])
+
+compressor = JinaRerank(model="jina-reranker-v2-base-multilingual", top_n=10)
+retriever = ContextualCompressionRetriever(
+    base_compressor=compressor, base_retriever=ensemble_retriever
+)
 
 
 def generate_query(state: SummaryState, config: RunnableConfig):
@@ -47,6 +105,31 @@ def generate_query(state: SummaryState, config: RunnableConfig):
             content = strip_thinking_tokens(content)
         search_query = content
     return {"search_query": search_query}
+
+
+def search(state: SummaryState, config: RunnableConfig):
+    configurable = Configuration.from_runnable_config(config)
+    results = retriever.invoke(f"{state.search_query}")
+    search_results = {"results": []}
+    for idx, doc in enumerate(results):
+        content = doc.page_content.replace("\n", " ")
+        url = doc.metadata.get("source", "")
+        title = doc.metadata.get("title", "")
+        search_results["results"].append({
+            "url": f"{url}:{title}",
+            "content": content,
+            "title": title
+        })
+    search_str = deduplicate_and_format_sources(
+        search_results,
+        max_tokens_per_source=1000,
+        fetch_full_page=False
+    )
+    return {
+        "sources_gathered": [format_sources(search_results)],
+        "search_loop_count": state.search_loop_count + 1,
+        "web_search_results": [search_str, ]
+    }
 
 
 def web_search(state: SummaryState, config: RunnableConfig):
@@ -121,8 +204,6 @@ def reflect_on_summary(state: SummaryState, config: RunnableConfig):
              f"And now identify a knowledge gap and generate a follow-up web search query:"))]
     )
     content = result.content
-    print(f"result: {content}")
-
     try:
         if content.startswith("```json"):
             content = extract_json_from_markdown(content)
@@ -150,28 +231,28 @@ def finalize_summary(state: SummaryState):
     return {"running_summary": state.running_summary}
 
 
-def route_search(state: SummaryState, config: RunnableConfig) -> Literal["finalize_summary", "web_search"]:
+def route_search(state: SummaryState, config: RunnableConfig) -> Literal["finalize_summary", "search"]:
     configurable = Configuration.from_runnable_config(config)
     if state.search_loop_count < configurable.max_web_search_loops:
-        return "web_search"
+        return "search"
     else:
         return "finalize_summary"
 
 
 builder = StateGraph(SummaryState, input=SummaryStateInput, output=SummaryStateOutput, config_schema=Configuration)
 builder.add_node("generate_query", generate_query)
-builder.add_node("web_search", web_search)
+builder.add_node("search", search)
 builder.add_node("summarize_sources", summarize_sources)
 builder.add_node("reflect_on_summary", reflect_on_summary)
 builder.add_node("finalize_summary", finalize_summary)
 
 builder.add_edge(START, "generate_query")
-builder.add_edge("generate_query", "web_search")
-builder.add_edge("web_search", "summarize_sources")
+builder.add_edge("generate_query", "search")
+builder.add_edge("search", "summarize_sources")
 builder.add_edge("summarize_sources", "reflect_on_summary")
 builder.add_conditional_edges("reflect_on_summary", route_search)
 builder.add_edge("finalize_summary", END)
 
 graph = builder.compile(debug=True)
-output = graph.invoke({"search_topic": "巴黎奥运会有哪些环保设计?"})
+output = graph.invoke({"search_topic": "北京申办过几次奥运会?"})
 print(output["running_summary"])
